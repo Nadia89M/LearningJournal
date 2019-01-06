b0ab7d874e85b49c3105517fad8e9a971cef58ed
Nadia <nadia.mohamed89@gmail.com>
2019-01-06 19:17:59 +0000

    Turbolink added to js file

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,43 @@
+$(document).on("turbolinks:load", function() {
+  $(".navbar .dropdown").hover(
+    function() {
+      $(this)
+        .find(".dropdown-menu")
+        .first()
+        .stop(true, true)
+        .delay(250)
+        .slideDown();
+    },
+    function() {
+      $(this)
+        .find(".dropdown-menu")
+        .first()
+        .stop(true, true)
+        .delay(100)
+        .slideUp();
+    }
+  );
+
+  $("#search-item").keyup(function() {
+    var value = $(this)
+      .val()
+      .toLowerCase()
+      .trim();
+    var items = $(".resource-item");
+    items.each(function() {
+      var type = $(this)
+        .text()
+        .toLowerCase()
+        .trim();
+      if (!type.includes(value)) {
+        $(this)
+          .parent()
+          .addClass("remove");
+      } else {
+        $(this)
+          .parent()
+          .removeClass("remove");
+      }
+    });
+  });
+});
