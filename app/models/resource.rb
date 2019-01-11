a1e78a9e321298ecee4b3282d5a14ac9a41a8946
Nadia89M <nadia.mohamed89@gmail.com>
2019-01-11 14:09:33 +0000

    Url validator

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,6 +3,7 @@ class Resource < ApplicationRecord
   validates :name, presence: true 
   validates :url, presence: true
   validates :memo, presence: true, length: { minimum: 5 }
+  validates :url, :format => URI::regexp(%w(http https))
   has_many :resource_languages
   has_many :languages, through: :resource_languages
   has_many_attached :images
