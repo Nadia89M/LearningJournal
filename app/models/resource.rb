cda0cad0f4e79fca382a5693b9ceea7c15e84907
Nadia89M <nadia.mohamed89@gmail.com>
2019-01-11 14:07:28 +0000

    Custom validator for Active Storage Images

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,7 +3,7 @@ class Resource < ApplicationRecord
   validates :name, presence: true 
   validates :url, presence: true
   validates :memo, presence: true, length: { minimum: 5 }
-  validates :images, image_format: true
+  validate :images_validation
   has_many :resource_languages
   has_many :languages, through: :resource_languages
   has_many_attached :images
@@ -15,4 +15,26 @@ class Resource < ApplicationRecord
   has_many :resource_dbs
   has_many :dbs, through: :resource_dbs
   belongs_to :project
+
+
+  private 
+
+  def images_validation
+    error_message = ''
+    images_valid = true
+    if images.attached?
+      images.each do |image|
+        if !image.blob.content_type.in?(%w(image/jpeg image/jpg image/png))
+          images_valid = false
+          error_message = 'The image wrong format'
+        end
+      end
+    end
+    unless images_valid
+      errors.add(:images, error_message)
+      self.images.purge
+      DestroyInvalidationRecordsJob.perform_later('images', 'Resource', self.id)
+    end
+  end
+
 end
--- app/validators/image_format_validator.rb
+++
@@ -1,8 +0,0 @@
-class ImageFormatValidator < ActiveStorage::EachValidator
-    
-    def validate_each(record, attribute, value)
-      unless value.include? "/jpg"
-        record.errors.add(attribute, "it's not an image")
-      end
-    end
-end
