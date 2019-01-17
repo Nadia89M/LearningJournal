5b721dd1b0c6c5ef633bda0c290499858ebe0ed3
Nadia89M <nadia.mohamed89@gmail.com>
2019-01-17 13:14:21 +0000

    Update Resorce Factory

--- spec/factories/resources.rb
+++ spec/factories/resources.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :resource do
-    name "Rspec"
-    memo "Rspec setup"
-    url "https://github.com/rspec/rspec-rails"
+    name { "Rspec" }
+    memo { "Rspec setup" }
+    url { "https://github.com/rspec/rspec-rails" }
     association :project 
   end
 end
